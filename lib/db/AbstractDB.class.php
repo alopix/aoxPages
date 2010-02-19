60f2df2dc8d7777863caa1ce7789c05022c7bccb
Dustin <dustin.steiner@gmail.com>
2010-02-19 13:54:35 +0100

    there's an abstract baseclass for all dbs

---
+++ lib/db/AbstractDB.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * aoxPages
+ * 
+ * a free as in beer homepage management system
+ * 
+ * @license		Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 Austria License
+ * @author		Dustin Steiner
+ * @link		http://www.alopix.net
+ * @email		dustin.steiner@gmail.com
+ * 
+ * @file		
+ * @version		1.0 Pre-Alpha 1
+ * @date		02/19/2010
+ * 
+ * Copyright (c) 2010
+ */
+
+/**
+ * AbstractDB class.
+ * 
+ */
+class AbstractDB {
+	/**
+	 * connection
+	 * 
+	 * @var mixed
+	 * @access protected
+	 */
+	protected $connection;
+}
+?>
